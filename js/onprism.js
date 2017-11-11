3571d39d59e76601051eba29b904fb65b9bc5caa
irucaice <csfn.skow@gmail.com>
2017-11-11 21:44:00 +0900

    add slider

---
+++ js/onprism.js
@@ -0,0 +1,22 @@
+var slider = tns({
+	container: '.my-slider',
+	items: 1,
+	edgePadding: 70,
+	speed: 600,
+	slideBy: 'page',
+	responsive: {
+		600: {
+		  items: 2
+		},
+		900: {
+		  items: 3
+		}
+  	},
+	mouseDrag: true,
+	navContainer: '#customize-thumbnails',
+	controls: false,
+//	autoplay: true,
+//	autoplayTimeout: 5000,
+//	autoplayButton: '#customize-toggle',
+//	autoplayText: ['▶','❚❚']
+  });
