022b56fc0a78986a6fc6f2dfb5925aaf9c173969
irucaice <csfn.skow@gmail.com>
2019-10-17 02:08:16 +0900

    [2019M3autumn]Pardalote first

---
+++ rpon0001/js/prhythmatic.js
@@ -0,0 +1,27 @@
+$(function() {
+	$("body").queryLoader2({
+        barColor: "rgb(60, 0, 50)",
+		backgroundColor: "#111",
+		barHeight: 2,
+	});
+
+    // ヘッダーメニューのスムーススクロール
+    const easeFunctions = {
+        easeInQuad: function (t, b, c, d) {
+            t /= d;
+            return c * t * t + b;
+        },
+        easeOutQuad: function (t, b, c, d) {
+            t /= d;
+            return -c * t* (t - 2) + b;
+        }
+    }
+    const moveTo = new MoveTo({
+        tolerance: $('header').height() - 1,
+        ease: 'easeInQuad'
+    }, easeFunctions);
+    const triggers = $('.js-trigger');
+    for (var i = 0; i < triggers.length; i++) {
+        moveTo.registerTrigger(triggers[i]);
+    }
+});
